08f37d758ad27c9dc0be256f1f186e631c6b3387
alston <wayanalston185@gmail.com>
2024-05-16 17:31:44 +0700

    add: post

--- script-test.js
+++ script-test.js
@@ -1,9 +1,31 @@
 import http from "k6/http";
+import { check, sleep } from 'k6';
+
+export const baseUrl = "https://test-api.k6.io/public/crocodiles/"
 
 export const options = {
-    iterations: 1,
+    stages:[
+        { duration: '10s', target: 10 },
+    ],
+} 
+
+export const params = {
+    header:{
+        'content-Type' : "application/json",
+    },
 }
 
+
+//payload signin
+const payload = JSON.stringify({
+    email : 'aaa',
+    password: 'bbb'
+})
+
+
 export default function (){
-    const reponse = http.get("https://test-api.k6.io/public/crocodiles/");
+    const response = http.post(baseUrl,payload,params)
+    check(response,{'status 200':(r)=> console.log(r)})
+    // check(response,{'status 200': (r) => r.status == 200})
+    // sleep(1)
 }
