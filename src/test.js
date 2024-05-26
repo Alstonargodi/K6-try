f2563d5188237a5c9806f130e696d845c37f7038
alston <wayanalston185@gmail.com>
2024-05-26 14:56:20 +0700

    add: stages

--- src/test.js
+++ src/test.js
@@ -2,18 +2,27 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
-  duration: '30s',
+  // // user dengan pengguna statis
+  // vus: 30,
+  // // durasi secara statis
+  // duration: '30s',
+
+  //penambahan user secara bertahap
+  stages:[
+    {duration:'10s',target:10},// 1. selama 10 detik bertahap sampai 10 vu
+    {duration:'10s',target:10},// 2. 10 detik kemudian masih 10 vu
+    {duration:'5s',target:20},//3. 5 detik kemudian naik 20 vu
+    {duration:'5s',target:0},//4. 5 detik kemudian turun menjadi 0 vu
+  ],
+
+  //options summary statistic
+  //p = percentil
+  summaryTrendStats:['avg','min','med','max','p(90)','p(95)']
 };
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
 
+//skenario testing
 export default function() {
   http.get('https://test.k6.io');
-  sleep(1);
+  // sleep(1); //sebelum berhenti, tunggu selama 1 detik
 }
